a03c3bd67a9e96975b7b80b2e6fbf5ae88d0218e
christanaomi <christanaomisilalahi@gmail.com>
2025-11-18 21:15:09 +0700

    CHRISTA

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,29 @@
-//Nama - NIM
+//Christa Naomi Silalahi-12S25041
+
+import java.util.*;
+import java.lang.Math;
+
+public class TLAST {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String judulTugas, kodeaMatkul, namaMatkul, dosen, deadLine, iDTugas;
+        int tingkatKesulitan, hariDeadline;
+        double prioritas;
+        String status;
+
+        JumlahData = 0;
+        // Name = input.nextLine();
+        // do {
+        // } while (Conditional);
+    }
+    
+    public static int addTask(int jumlahData) {
+        if (jumlahData < 10) {
+            // Name = input.nextLine();
+        }
+        
+        return ;
+    }
+}
 
